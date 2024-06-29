887b013f1da88509c5fd6213db01bb98c3eaa35f
Your Name <sakshyambanjade@gmail.com>
2024-06-29 18:18:04 +0545

    added micronaut

---
+++ umanager/src/test/java/umanager/UmanagerTest.java
@@ -0,0 +1,21 @@
+package umanager;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class UmanagerTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
